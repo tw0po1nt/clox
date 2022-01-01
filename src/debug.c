e3d122b34e09f8f512901fb614116d09be9aa227
Matthew Watt <matt@twopoint.dev>
2022-01-01 15:57:40 -0600

    Optimize line encoding

--- src/debug.c
+++ src/debug.c
@@ -29,10 +29,11 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset) {
 
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
-    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
+    int line = getLine(chunk, offset);
+    if (offset > 0 && line == getLine(chunk, offset - 1)) {
         printf("%4s ", "|");
     } else {
-        printf("%4d ", chunk->lines[offset]);
+        printf("%4d ", line);
     }
 
     uint8_t instruction = chunk->code[offset];
